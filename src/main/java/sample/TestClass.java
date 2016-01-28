a1906c0a5b8f3666267744fa878d409e57150f68
kjcommon138 <kjcommon@ncsu.edu>
2016-01-28 09:38:35 -0500

    Added missing src files

---
+++ src/main/java/sample/TestClass.java
@@ -0,0 +1,12 @@
+package sample;
+
+/**
+ * Created by michael on 1/26/16.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello from java");
+    }
+
+}
