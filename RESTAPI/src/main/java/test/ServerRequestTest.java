b90a1a6b189dc4361cb560fe0cf927b5d4006db9
latimko <latimko@ncsu.edu>
2016-02-26 00:14:37 -0500

    Add some JUnit tests to the REST API.

---
+++ RESTAPI/src/main/java/test/ServerRequestTest.java
@@ -0,0 +1,63 @@
+package test;
+
+import hello.*;
+
+import org.junit.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by laureltimko on 2/25/16.
+ */
+public class ServerRequestTest {
+
+    Server server1;
+    ServerRequest request;
+
+    @Before
+    public void setUp() throws Exception {
+        request = new ServerRequest();
+        server1 = new Server();
+        server1.setHost("111.22.33.444");
+        server1.setPort(1000);
+
+    }
+
+
+    @Test
+    public void testGetServer() throws Exception {
+        request.setServer(server1);
+        assertEquals(request.getServer(), server1);
+    }
+
+    @Test
+    public void testSetServer() throws Exception {
+        request.setServer(server1);
+        assertEquals(request.getServer(), server1);
+    }
+
+    @Test
+    public void testGetServerRemove() throws Exception {
+        request.setServerRemove(server1);
+        assertEquals(request.getServerRemove(), server1);
+    }
+
+    @Test
+    public void testSetServerRemove() throws Exception {
+        request.setServerRemove(server1);
+        assertEquals(request.getServerRemove(), server1);
+    }
+
+    @Test
+    public void testGetServerAdd() throws Exception {
+        request.setServerAdd(server1);
+        assertEquals(request.getServerAdd(), server1);
+    }
+
+    @Test
+    public void testSetServerAdd() throws Exception {
+        request.setServerAdd(server1);
+        assertEquals(request.getServerAdd(), server1);
+    }
+
+}
---
+++ RESTAPI/src/main/java/test/ServerTest.java
@@ -0,0 +1,113 @@
+package test;
+
+import hello.Server;
+
+import org.junit.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by laureltimko on 2/25/16.
+ */
+public class ServerTest {
+
+    Server myServer = new Server();
+
+    @Test
+    public void testSetBeginningSlot() throws Exception {
+        myServer.setBeginningSlot(1001);
+        assertEquals(myServer.getBeginningSlot(), 1001);
+    }
+
+    @Test
+    public void testGetBeginningSlot() throws Exception {
+        myServer.setBeginningSlot(2001);
+        assertEquals(myServer.getBeginningSlot(), 2001);
+    }
+
+    @Test
+    public void testSetEndSlot() throws Exception {
+        myServer.setEndSlot(1001);
+        assertEquals(myServer.getEndSlot(), 1001);
+    }
+
+    @Test
+    public void testGetEndSlot() throws Exception {
+        myServer.setEndSlot(2001);
+        assertEquals(myServer.getEndSlot(), 2001);
+    }
+
+    @Test
+    public void testSetPort() throws Exception {
+        myServer.setPort(30001);
+        assertEquals(myServer.getPort(), 30001);
+    }
+
+    @Test
+    public void testGetPort() throws Exception {
+        myServer.setPort(30002);
+        assertEquals(myServer.getPort(), 30002);
+    }
+
+    @Test
+    public void testSetKey() throws Exception {
+        myServer.setKey("test");
+        assertEquals(myServer.getKey(), "test");
+    }
+
+    @Test
+    public void testGetKey() throws Exception {
+        myServer.setKey("test2");
+        assertEquals(myServer.getKey(), "test2");
+    }
+
+    @Test
+    public void testSetType() throws Exception {
+        myServer.setType("test");
+        assertEquals(myServer.getType(), "test");
+    }
+
+    @Test
+    public void testGetType() throws Exception {
+        myServer.setType("test2");
+        assertEquals(myServer.getType(), "test2");
+    }
+
+    @Test
+    public void testSetHost() throws Exception {
+        myServer.setHost("111.11.11.111");
+        assertEquals(myServer.getHost(), "111.11.11.111");
+    }
+
+    @Test
+    public void testGetHost() throws Exception {
+        myServer.setHost("111.22.33.444");
+        assertEquals(myServer.getHost(), "111.22.33.444");
+    }
+
+    @Test
+    public void testSetNodeID() throws Exception {
+        myServer.setNodeID("node1");
+        assertEquals(myServer.getNodeID(), "node1");
+    }
+
+    @Test
+    public void testGetNodeID() throws Exception {
+        myServer.setNodeID("node2");
+        assertEquals(myServer.getNodeID(), "node2");
+    }
+
+    @Test
+    public void testSetServerInfo() throws Exception {
+        myServer.setServerInfo("item1, item2, item3");
+        assertEquals(myServer.getServerInfo(), "item1, item2, item3");
+    }
+
+    @Test
+    public void testGetServerInfo() throws Exception {
+        myServer.setServerInfo("item1, item2");
+        assertEquals(myServer.getServerInfo(), "item1, item2");
+    }
+
+
+}
