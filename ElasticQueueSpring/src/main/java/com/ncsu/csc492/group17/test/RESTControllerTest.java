518eac4590b430f988e41b26511ed6fc5c6cade2
latimko <latimko@ncsu.edu>
2016-03-03 16:36:34 -0500

    Copied tests from Dashboard and REST API into Spring version

---
+++ ElasticQueueSpring/src/main/java/com/ncsu/csc492/group17/test/RESTControllerTest.java
@@ -0,0 +1,104 @@
+package com.ncsu.csc492.group17.test;
+
+import com.ncsu.csc492.group17.web.model.*;
+import com.ncsu.csc492.group17.web.controller.*;
+
+import org.junit.*;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by laureltimko on 3/3/16.
+ */
+public class RESTControllerTest {
+    RESTController controller;
+    Server server1;
+    ServerRequest request;
+
+    @Before
+    public void setUp() throws Exception {
+        controller = new RESTController();
+        request = new ServerRequest();
+        server1 = new Server();
+        server1.setHost("152.14.106.29");
+        server1.setPort(6000);
+
+    }
+
+    @Test
+    public void testGetServers() {
+        List<Server> list = controller.getServers(server1);
+        assertEquals(4, list.size());
+        Server testServer = list.get(0);
+
+        //Check all servers
+        assertEquals(testServer.getHost(), "152.14.106.29");
+        assertEquals(testServer.getPort(), 6003);
+
+        testServer = list.get(1);
+        assertEquals(testServer.getHost(), "152.14.106.29");
+        assertEquals(testServer.getPort(), 6000);
+
+        testServer = list.get(2);
+        assertEquals(testServer.getHost(), "152.14.106.29");
+        assertEquals(testServer.getPort(), 6002);
+
+        testServer = list.get(3);
+        assertEquals(testServer.getHost(), "152.14.106.29");
+        assertEquals(testServer.getPort(), 6001);
+    }
+
+    @Test
+    public void testGetQueues() {
+        List<String> list = controller.getQueues(server1);
+        assertEquals(1, list.size());
+        assertEquals("myList2", list.get(0));
+    }
+
+    @Test
+    public void testGetItems() {
+        server1.setKey("myList2");
+        List<String> list = controller.getItems(server1);
+        assertEquals(9, list.size());
+        assertEquals("item2", list.get(0));
+        assertEquals("item1", list.get(1));
+        assertEquals("test", list.get(2));
+        assertEquals("f", list.get(3));
+        assertEquals("e", list.get(4));
+        assertEquals("d", list.get(5));
+        assertEquals("c", list.get(6));
+        assertEquals("b", list.get(7));
+        assertEquals("a", list.get(8));
+    }
+
+    @Test
+    public void testRemoveSlots() {
+        Server.Slots s = new Server.Slots();
+        s.setBeginningSlot(12000);
+        s.setEndSlot(13000);
+
+        Server.Slots slot[] = new Server.Slots[1];
+        slot[0] = s;
+
+        server1.setSlots(slot);
+        String message = controller.removeSlots(server1);
+        assertEquals("Slots 12000 to 13000 removed from 152.14.106.29:6000", message);
+    }
+
+    @Test
+    public void testAddSlots() {
+        Server.Slots s = new Server.Slots();
+        s.setBeginningSlot(12000);
+        s.setEndSlot(13000);
+
+        Server.Slots slot[] = new Server.Slots[1];
+        slot[0] = s;
+
+        server1.setSlots(slot);
+        String message = controller.addSlots(server1);
+        assertEquals("Slots 12000 to 13000 added to 152.14.106.29:6000", message);
+    }
+}
---
+++ ElasticQueueSpring/src/main/java/com/ncsu/csc492/group17/test/ServerTest.java
@@ -0,0 +1,153 @@
+package com.ncsu.csc492.group17.test;
+
+import com.ncsu.csc492.group17.web.model.Server;
+import com.ncsu.csc492.group17.web.model.Server.Slots;
+
+import org.junit.*;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by laureltimko on 3/3/16.
+ */
+public class ServerTest {
+
+    Server myServer = new Server();
+    Slots slot[] = new Slots[1];
+
+    @Test
+    public void testSetBeginningSlot() throws Exception {
+        Slots s = new Slots();
+        s.setBeginningSlot(1001);
+        assertEquals(s.getBeginningSlot(), 1001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        Slots results[] = myServer.getSlots();
+        assertEquals(results[0].getBeginningSlot(), 1001);
+    }
+
+    @Test
+    public void testGetBeginningSlot() throws Exception {
+        Slots s = new Slots();
+        s.setBeginningSlot(2001);
+        assertEquals(s.getBeginningSlot(), 2001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        Slots results[] = myServer.getSlots();
+        assertEquals(results[0].getBeginningSlot(), 2001);
+    }
+
+    @Test
+    public void testSetEndSlot() throws Exception {
+        Slots s = new Slots();
+        s.setEndSlot(1001);
+        assertEquals(s.getEndSlot(), 1001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        Slots results[] = myServer.getSlots();
+        assertEquals(results[0].getEndSlot(), 1001);
+    }
+
+    @Test
+    public void testGetEndSlot() throws Exception {
+        Slots s = new Slots();
+        s.setEndSlot(2001);
+        assertEquals(s.getEndSlot(), 2001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        Slots results[] = myServer.getSlots();
+        assertEquals(results[0].getEndSlot(), 2001);
+    }
+
+    @Test
+    public void testSetSlots() throws Exception {
+        Slots s = new Slots();
+        s.setBeginningSlot(1001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        assertEquals(myServer.getSlots(), slot);
+        assertEquals(myServer.getSlots()[0].getBeginningSlot(), 1001);
+    }
+
+    @Test
+    public void testGetSlots() throws Exception {
+        Slots s = new Slots();
+        s.setEndSlot(2001);
+        slot[0] = s;
+        myServer.setSlots(slot);
+        assertEquals(myServer.getSlots(), slot);
+        assertEquals(myServer.getSlots()[0].getEndSlot(), 2001);
+    }
+
+    @Test
+    public void testSetPort() throws Exception {
+        myServer.setPort(30001);
+        assertEquals(myServer.getPort(), 30001);
+    }
+
+    @Test
+    public void testGetPort() throws Exception {
+        myServer.setPort(30002);
+        assertEquals(myServer.getPort(), 30002);
+    }
+
+    @Test
+    public void testSetKey() throws Exception {
+        myServer.setKey("test");
+        assertEquals(myServer.getKey(), "test");
+    }
+
+    @Test
+    public void testGetKey() throws Exception {
+        myServer.setKey("test2");
+        assertEquals(myServer.getKey(), "test2");
+    }
+
+    @Test
+    public void testSetType() throws Exception {
+        myServer.setType("test");
+        assertEquals(myServer.getType(), "test");
+    }
+
+    @Test
+    public void testGetType() throws Exception {
+        myServer.setType("test2");
+        assertEquals(myServer.getType(), "test2");
+    }
+
+    @Test
+    public void testSetHost() throws Exception {
+        myServer.setHost("111.11.11.111");
+        assertEquals(myServer.getHost(), "111.11.11.111");
+    }
+
+    @Test
+    public void testGetHost() throws Exception {
+        myServer.setHost("111.22.33.444");
+        assertEquals(myServer.getHost(), "111.22.33.444");
+    }
+
+    @Test
+    public void testSetNodeID() throws Exception {
+        myServer.setNodeID("node1");
+        assertEquals(myServer.getNodeID(), "node1");
+    }
+
+    @Test
+    public void testGetNodeID() throws Exception {
+        myServer.setNodeID("node2");
+        assertEquals(myServer.getNodeID(), "node2");
+    }
+
+    @Test
+    public void testSetServerInfo() throws Exception {
+        myServer.setServerInfo("item1, item2, item3");
+        assertEquals(myServer.getServerInfo(), "item1, item2, item3");
+    }
+
+    @Test
+    public void testGetServerInfo() throws Exception {
+        myServer.setServerInfo("item1, item2");
+        assertEquals(myServer.getServerInfo(), "item1, item2");
+    }
+}
