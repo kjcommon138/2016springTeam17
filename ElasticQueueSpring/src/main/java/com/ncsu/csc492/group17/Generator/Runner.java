0912678043ea223191fc1f5d9017096eda331b84
Michael Goff <magoff2@ncsu.edu>
2016-04-05 09:40:38 -0400

    Removing QueueGenerator from ElasticQueueSpring

--- ElasticQueueSpring/src/main/java/com/ncsu/csc492/group17/Generator/Runner.java
+++
@@ -1,8 +0,0 @@
-package com.ncsu.csc492.group17.Generator;
-
-public class Runner {
-    public static void main(String args[]) {
-        QueueGenerator gen = new QueueGenerator();
-        gen.run();
-    }
-}
